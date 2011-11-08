dc1f0e9f3448344a813da0702813ce09d6991a1d
Michael <mars8082686@gmail.com>
2011-11-09 00:40:08 +0900

    Added message controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyMaster::Application.config.secret_token = '825e7f9fe302c11a44fd83d03f6577ea4e79a207f5b34e2c9cd8a99fc7b7fc034a9143a18b03dd92c8db44297df4d2bd1bbe8be4a78e11bf44026cc75071b98b'
