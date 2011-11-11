ba5b3ab200f57a3a8f9c8075970d3e2f9311d459
Michael <mars8082686@gmail.com>
2011-11-12 03:48:33 +0900

    removed index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 RubyMaster::Application.routes.draw do
+  match '/',        :to => 'pages#home'
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
